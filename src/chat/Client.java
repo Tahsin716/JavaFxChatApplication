66709bb5e4a7156e8afbc2f75af1a1af1191a244
Tahsin Rashad <tahsin.rashad716@gmail.com>
2017-01-09 02:24:13 +0600

    Server Class updated

--- src/chat/Client.java
+++ src/chat/Client.java
@@ -2,5 +2,13 @@ package chat;
 
 public class Client extends NetworkConnection {
 
+    private int portNumber;
+    private String IP;
 
+    /**
+     * Public constructor for Client, super(onReceiveCallback) is used to call the
+     * constructor of the super class.
+     */
+
+    public Client()
 }
--- src/chat/Server.java
+++ src/chat/Server.java
@@ -1,5 +1,39 @@
 package chat;
 
+import java.io.Serializable;
+import java.util.function.Consumer;
+
 public class Server extends NetworkConnection {
 
+    private int portNumber;
+
+    /**
+     * Public constructor for Server, super(onReceiveCallback) is used to call the
+     * constructor of the super class.
+    */
+
+    public Server(int portNumber, Consumer<Serializable> onReceiveCallback) {
+        super(onReceiveCallback);
+        this.portNumber = portNumber;
+    }
+
+
+    /**
+     * Abstract methods overriden
+    */
+
+    @Override
+    protected boolean isServer() {
+        return true;
+    }
+
+    @Override
+    protected String getIP() {
+        return null;
+    }
+
+    @Override
+    protected int getPort() {
+        return portNumber;
+    }
 }
