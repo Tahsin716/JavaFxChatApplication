b00bd8e178be4c798cb402e7f434da53ed950251
Tahsin Rashad <tahsin.rashad716@gmail.com>
2017-01-10 09:17:54 +0600

    Controller Class update - 1

--- src/chat/Controller.java
+++ src/chat/Controller.java
@@ -1,5 +1,18 @@
 package chat;
 
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.TextArea;
+
 public class Controller {
+
+    //Referring to the display Text Area
+    @FXML
+    TextArea display;
+    //Referring to the input Text Area
+    @FXML
+    TextArea input;
+
+
 
 }
