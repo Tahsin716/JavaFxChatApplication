26a208e726755f18929f8d56606a2c1bdbfc8821
Tahsin Rashad <tahsin.rashad716@gmail.com>
2017-01-10 10:38:38 +0600

    Main class update - 2

--- src/chat/Main.java
+++ src/chat/Main.java
@@ -1,12 +1,21 @@
 package chat;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    @FXML
+    TextArea display;
+
+    @FXML
+    TextArea input;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -16,6 +25,49 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+
+    //Determines whether server of client is to be created
+    public static boolean isServer = false;
+
+
+    //Connection should be either server or client
+    public  NetworkConnection connection = isServer ? createServer() : createClient();
+
+
+    /**
+     * Creates connection as server
+     *
+     * @return
+     */
+
+    private Server createServer() {
+        return new Server(50000, data -> {
+            Platform.runLater(() -> {
+                display.appendText(data.toString() + "\n");
+            });
+        });
+    }
+
+    /**
+     * Creates connection as client
+     *
+     * @return
+     */
+
+    private Client createClient() {
+        return new Client("27.147.226.68", 50000, data -> {
+            Platform.runLater(() -> {
+                display.appendText(data.toString() + "\n");
+            });
+        });
+    }
+
+
+    /**
+     * Launches the program
+     *
+     * @param args
+     */
 
     public static void main(String[] args) {
         launch(args);
